3f6c33958a4bab9069b76e6fb095478551862980
Nerfeali-1 <nerfeali100172@gmail.com>
2025-04-01 12:44:31 -0600

    uwu

--- AlquimiaDragon/js/usuarios.js
+++ AlquimiaDragon/js/usuarios.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+git  document.addEventListener("DOMContentLoaded", function () {
     cargarUsuarios();
 
     // Agregar evento de búsqueda
