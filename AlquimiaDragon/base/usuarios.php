9e50cd344d41b5ec7f79fe6f74a3a3da18240c97
Nerfeali-1 <nerfeali100172@gmail.com>
2025-04-01 12:11:41 -0600

    update uwu

--- AlquimiaDragon/base/usuarios.php
+++ AlquimiaDragon/base/usuarios.php
@@ -4,7 +4,7 @@ include("conexion.php");
 $action = $_GET['action'];
 
 if ($action === 'fetch') {
-    $query = "SELECT ID_usuario, Nombre, Correo FROM Usuario2 WHERE Rol != 2";  // Excluir administradores
+    $query = "SELECT ID_usuario, Nombre, Correo FROM Usuario2 WHERE ID_rol != 10";  // Excluir administradores
     $result = $conn->query($query);
 
     $usuarios = [];
@@ -15,7 +15,7 @@ if ($action === 'fetch') {
     echo json_encode($usuarios);
 } elseif ($action === 'delete' && isset($_GET['id'])) {
     $idUsuario = $_GET['id'];
-    $deleteQuery = "DELETE FROM Usuario2 WHERE ID_usuario = $idUsuario AND Rol != 2";
+    $deleteQuery = "DELETE FROM Usuario2 WHERE ID_usuario = $idUsuario AND ID_rol != 10";
 
     if ($conn->query($deleteQuery) === TRUE) {
         echo "Usuario eliminado correctamente.";
